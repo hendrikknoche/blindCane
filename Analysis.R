6ea30cd3f964bbae5afb366bffce67c513ee2ebb
Hendrik Knoche <hendrik.knoche@gmail.com>
2020-05-13 15:48:39 +0200

    data rearranged

--- Analysis.R
+++ Analysis.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(readbulk)
-df = readbulk::read_bulk('results_final', sep=';', na.strings = 'none', stringsAsFactors=FALSE)
+df = readbulk::read_bulk('data', sep=';', na.strings = 'none', stringsAsFactors=FALSE)
+df$testID<-as.numeric(gsub("[^0-9.-]", "", substr(df$File,7,10)))
 df$timeSinceStart<-as.double(substr(df$Timer,9,16))*1000
-
+df<-df[,c(18:21,23,1:17,22)]
 df %>% group_by(Test.ID,FOD,Scenario)%>%summarize(sumOfTime=sum(timeSinceStart))
