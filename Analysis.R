d8237ab1d81c8de2870f23a6d407a477c73fe5eb
Hendrik Knoche <hendrik.knoche@gmail.com>
2020-05-14 20:44:08 +0200

    agg by FDR plus plot

--- Analysis.R
+++ Analysis.R
@@ -3,6 +3,13 @@ library(readbulk)
 library(lubridate)
 
 
+lower_ci <- function(mean, se, n, conf_level = 0.95){
+  lower_ci <- mean - qt(1 - ((1 - conf_level) / 2), n - 1) * se
+}
+upper_ci <- function(mean, se, n, conf_level = 0.95){
+  upper_ci <- mean + qt(1 - ((1 - conf_level) / 2), n - 1) * se
+}
+
 load('data_all.rda') 
 # helper= read.csv('DataRepair.csv',sep=";")
 # daggByScen<-merge(daggByScen,helper)
@@ -21,6 +28,9 @@ daggByScen <-daggByScen%>%group_by(FOD,day,Range)%>%mutate(timeFDRtrain=round(cu
 daggByScen <-daggByScen%>%group_by(FOD,day)%>%mutate(timeFDtrain=round(cumsum(Time)))
 daggByScen <-daggByScen%>%group_by(day)%>%mutate(timeDtrain=round(cumsum(Time)))
 
+daggByDFR<-daggByScen %>%group_by(FOD,day,Range)%>%summarize(totalTimeTraining=max(totalTimeTraining),avgTime=mean(Time),ssd = sd(Time),count=n(),se = ssd / sqrt(count))%>%mutate(lower_ci=lower_ci(avgTime,se,count),upper_ci=upper_ci(avgTime,se,count))
+
+
 doa<-daggByScen %>%group_by(day,FOD,Range)%>%summarize(Time=max(Time),speedSD=sd(avgSpeed),avgSpeed=mean(avgSpeed),maxSpeed=max(maxSpeed))
 
 ggplot(daggByScen,aes(x=FOD,y=Time,color=Range))+geom_point(aes(alpha=.1))
@@ -30,3 +40,5 @@ ggplot(daggByScen,aes(x=totalTimeTraining,y=Time,color=FOD))+geom_point()+ geom_
   stat_smooth(aes(color="red"),method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+theme_bw()+facet_grid(cols=vars(FOD))
 
                                                                                          
+ggplot(daggByDFR,aes(x=totalTimeTraining,y=avgTime,color=factor(Range),shape=factor(Range)))+geom_point()+ geom_smooth(size=0)+ geom_errorbar(aes(ymin=lower_ci, ymax=upper_ci))+
+  stat_smooth(method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+theme_bw()+facet_grid(cols=vars(FOD))
