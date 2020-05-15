6f995f5f7a94be8c8f5c432d430184f2837c5873
Marsfeldt <mskovf14@student.aau.dk>
2020-05-15 17:54:55 +0200

    Clean-up of plots

--- Analysis.R
+++ Analysis.R
@@ -60,99 +60,171 @@ ggplot(daggByScen,aes(x=Scenario,y=maxSpeed,color=Range))+
 #----------------------------   The effect of detections
 
 #Number of detections effect on time
-ggplot(daggByScen,aes(x = Time, y = objectDetected, color = FOD, size = Range))+
+ggplot(daggByScen,aes(y = Time, x = objectDetected, color = factor(Range)))+
   geom_point()+ 
-  geom_smooth(size=0)+ 
-  stat_smooth(aes(color="red"),method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)
+  geom_jitter()+
+  #geom_smooth(size=0)+ 
+  stat_smooth(color="red",method = 'nls', formula = 'y~a+x*b', method.args = list(start= c(a = 1,b=1)),se=FALSE)
 
 #Number of detections effect on time split per FOD
-ggplot(daggByScen,aes(x = Time, y = objectDetected, color = Range))+
-  geom_point(aes(alpha=.1))+ 
-  geom_smooth(size=0)+
-  stat_smooth(method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
+ggplot(daggByScen,aes(y = Time, x = objectDetected, color = factor(Range)))+
+  geom_point(aes(alpha=.1))+
+  geom_jitter()+
+  #geom_smooth(size=0)+
+  stat_smooth(method = 'nls', formula = 'y~a+x*b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
   theme_bw()+
   facet_grid(cols=vars(FOD))
 
 #Number of detections effect on avgSpeed 
-ggplot(daggByScen,aes(x = avgSpeed, y = objectDetected, color = FOD))+
+ggplot(daggByScen,aes(y = avgSpeed, x = objectDetected, color = factor(Range)))+
   geom_point()+ 
-  geom_smooth(size=0)+ 
-  stat_smooth(aes(color="red"),method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)
+  geom_jitter()+
+  #geom_smooth(size=0)+ 
+  stat_smooth(color="red",method = 'nls', formula = 'y~a+x*b', method.args = list(start= c(a = 1,b=1)),se=FALSE)
 
 #Number of detections effect on avgSpeed split per FOD
-ggplot(daggByScen,aes(x = avgSpeed, y = objectDetected, color = Range))+
-  geom_point(aes(alpha=.1))+ 
-  geom_smooth(size=0)+
-  stat_smooth(method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
+ggplot(daggByScen,aes(y = avgSpeed, x = objectDetected, color = factor(Range)))+
+  geom_point(aes(alpha=.1))+
+  geom_jitter()+
+  #geom_smooth(size=0)+
+  stat_smooth(method = 'nls', formula = 'y~a+x*b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
   theme_bw()+
   facet_grid(cols=vars(FOD))
 
 #----------------------------   The effect of Collisions
 
 #Number of Collisions effect on time
-ggplot(daggByScen,aes(x = Time, y = objectCollisions, color = FOD))+
+ggplot(daggByScen,aes(y = Time, x = objectCollisions, color = factor(Range)))+
   geom_point()+ 
-  geom_smooth(size=0)+ 
-  stat_smooth(aes(color="red"),method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)
+  geom_jitter()+
+  #geom_smooth(size=0)+ 
+  stat_smooth(color="red",method = 'nls', formula = 'y~a+x*b', method.args = list(start= c(a = 1,b=1)),se=FALSE)
 
 #Number of Collisions effect on time split per FOD
-ggplot(daggByScen,aes(x = Time, y = objectCollisions, color = Range))+
+ggplot(daggByScen,aes(y = Time, x = objectCollisions, color = factor(Range)))+
   geom_point(aes(alpha=.1))+ 
-  geom_smooth(size=0)+
-  stat_smooth(method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
+  geom_jitter()+
+  #geom_smooth(size=0)+
+  stat_smooth(method = 'nls', formula = 'y~a+x*b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
   theme_bw()+
   facet_grid(cols=vars(FOD))
 
 #Number of Collisions effect on avgSpeed
-ggplot(daggByScen,aes(x = avgSpeed, y = objectCollisions, color = FOD))+
+ggplot(daggByScen,aes(y = avgSpeed, x = objectCollisions, color = factor(Range)))+
   geom_point()+ 
-  geom_smooth(size=0)+ 
-  stat_smooth(aes(color="red"),method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)
+  geom_jitter()+
+  #geom_smooth(size=0)+ 
+  stat_smooth(aes(color="red"),method = 'nls', formula = 'y~a+x*b', method.args = list(start= c(a = 1,b=1)),se=FALSE)
 
 #Number of Collisions effect on avgSpeed split per FOD
-ggplot(daggByScen,aes(x = avgSpeed, y = objectCollisions, color = Range))+
+ggplot(daggByScen,aes(y = avgSpeed, x = objectCollisions, color = factor(Range)))+
   geom_point(aes(alpha=.1))+ 
-  geom_smooth(size=0)+
-  stat_smooth(method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
+  geom_jitter()+
+  #geom_smooth(size=0)+
+  stat_smooth(method = 'nls', formula = 'y~a+x*b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
   theme_bw()+
   facet_grid(cols=vars(FOD))
 
 #----------------------------   The effect of Collisions and detection
 
 #Number of detections and collisions based on avgSpeed split by FOD
-ggplot(daggByScen,aes(x = avgSpeed, y = objectCollisions, colour = FOD))+
+ggplot(daggByScen,aes(y = avgSpeed, x = objectCollisions, colour = factor(FOD)))+
   geom_point()+
+  geom_jitter()+
   #geom_smooth(size=0, color = "blue")+ 
-  stat_smooth(color="red",method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
-  geom_point(aes(x=avgSpeed, y=objectDetected), color = "green")+
+  stat_smooth(color="red",method = 'nls', formula = 'y~a+x*b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
+  geom_point(aes(x = objectDetected, colour = factor(FOD)))+
   #geom_smooth(aes(x = avgSpeed, y = objectDetected))+
-  stat_smooth(aes(x = avgSpeed, y = objectDetected), color="red",method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
+  stat_smooth(aes(x = objectDetected), color="red",method = 'nls', formula = 'y~a+x*b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
   facet_grid(cols=vars(Range))
 
 #Number of detections and collisions based on avgSpeed split by Range
-ggplot(daggByScen,aes(x = avgSpeed, y = objectCollisions, colour = Range))+
+ggplot(daggByScen,aes(y = avgSpeed, x = objectCollisions, colour = factor(Range)))+
   geom_point()+
+  geom_jitter()+
   #geom_smooth(size=0, color = "blue")+ 
-  stat_smooth(color="red",method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
-  geom_point(aes(x=avgSpeed, y=objectDetected), color = "green")+
+  stat_smooth(color="red",method = 'nls', formula = 'y~a+x*b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
+  geom_point(aes(x = objectDetected, colour = factor(Range)))+
   #geom_smooth(aes(x = avgSpeed, y = objectDetected))+
-  stat_smooth(aes(x = avgSpeed, y = objectDetected), color="red",method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
+  stat_smooth(aes(x = objectDetected), color="red",method = 'nls', formula = 'y~a+x*b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
   facet_grid(cols=vars(FOD))
 
 #Number of detections and collisions based on avgSpeed split by FOD and Range
-ggplot(daggByScen,aes(x = avgSpeed, y = objectCollisions))+
+ggplot(daggByScen,aes(y = avgSpeed, x = objectCollisions))+
   geom_point(color="blue")+
+  geom_jitter()+
   #geom_smooth(size=0, color = "blue")+ 
-  stat_smooth(color="red",method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
-  geom_point(aes(x=avgSpeed, y=objectDetected), color = "green")+
+  stat_smooth(color="red",method = 'nls', formula = 'y~a+x*b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
+  geom_point(aes(x = objectDetected), color = "green")+
   #geom_smooth(aes(x = avgSpeed, y = objectDetected))+
-  stat_smooth(aes(x = avgSpeed, y = objectDetected), color="red",method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
+  stat_smooth(aes(x = objectDetected), color="red",method = 'nls', formula = 'y~a+x*b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
   facet_grid(cols=vars(FOD), rows=vars(Range))
+
+#----------------------------   Time of compleation vs avgSpeed
+
+#Number of Collisions effect on time
+ggplot(daggByScen,aes(x = Time, y = avgSpeed, color = factor(Range)))+
+  geom_point()+ 
+  geom_jitter()+
+  #geom_smooth(size=0)+ 
+  stat_smooth(color="red",method = 'nls', formula = 'y~a+x*b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
+  stat_smooth(color="blue",method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)
+
+#Number of Collisions effect on time split per FOD
+ggplot(daggByScen,aes(x = Time, y = avgSpeed, color = factor(Range)))+
+  geom_point(aes(alpha=.1))+ 
+  geom_jitter()+
+  #geom_smooth(size=0)+
+  stat_smooth(method = 'nls', formula = 'y~a+x*b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
+  theme_bw()+
+  facet_grid(cols=vars(FOD))
+
+#Number of Collisions effect on time split per FOD and per day
+ggplot(daggByScen,aes(x = Time, y = avgSpeed, color = factor(Range)))+
+  geom_point(aes(alpha=.1))+ 
+  geom_jitter()+
+  #geom_smooth(size=0)+
+  stat_smooth(method = 'nls', formula = 'y~a+x*b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
+  theme_bw()+
+  facet_grid(cols=vars(FOD), row=vars(day))
+
+#----------------------------   Time of compleation vs total time
+
+#time vs total time split by FOD
+ggplot(daggByScen,aes(x = totalTimeTraining, y = Time, color = factor(Range)))+
+  geom_point()+ 
+  geom_jitter()+
+  #geom_smooth(size=0)+ 
+  stat_smooth(aes(color="red"),method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
+  theme_bw()
+
+#time vs total time split by FOD
+ggplot(daggByScen,aes(x = totalTimeTraining, y = Time, color = factor(Range)))+
+  geom_point()+ 
+  geom_jitter()+
+  #geom_smooth(size=0)+ 
+  stat_smooth(aes(color="red"),method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
+  theme_bw()+
+  facet_grid(cols=vars(FOD))
+
+#time vs total time split by FOD and day
+ggplot(daggByScen,aes(x = totalTimeTraining, y = Time, color = factor(Range)))+
+  geom_point()+ 
+  geom_jitter()+
+  #geom_smooth(size=0)+ 
+  stat_smooth(aes(color="red"),method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
+  theme_bw()+
+  facet_grid(cols=vars(FOD), row = vars(day))
+
+#----------------------------   The effect of range
+
+ggplot(daggByScen,aes(x=FOD,y=Time,color=Range))+
+  geom_point(aes(alpha=.1))
 
 #----------------------------   Heatmap Table
 
 #Data Frame for heatmap table
-daggHeat <- df %>% filter(Person_Speed<3)%>%group_by(Scenario,FOD,Range)%>%summarize(avgSpeed=mean(Person_Speed),medianSpeed=median(Person_Speed),maxSpeed=max(Person_Speed),minSpeed=min(Person_Speed),objectDetected=sum(objDet,na.rm = TRUE),objectCollisions=sum(objColl,na.rm = TRUE),Time=max(Time_in_MS*1000))
+daggHeat <- df %>% filter(Person_Speed<3)%>%group_by(Scenario,FOD,Range,day)%>%summarize(avgSpeed=mean(Person_Speed),medianSpeed=median(Person_Speed),maxSpeed=max(Person_Speed),minSpeed=min(Person_Speed),objectDetected=sum(objDet,na.rm = TRUE),objectCollisions=sum(objColl,na.rm = TRUE),Time=max(Time_in_MS*1000))
 
 #Heatmap table over avgSpeed
 ggplot(daggHeat, aes(x = Range, y = Scenario)) + 
@@ -174,7 +246,7 @@ ggplot(daggHeat, aes(x = Range, y = Scenario)) +
   theme(legend.title=element_text(face="bold", size=14)) + 
   scale_y_continuous(trans = "reverse")+
   labs(fill="avgSpeed") +
-  facet_grid(cols=vars(FOD))
+  facet_grid(cols=vars(FOD), row=vars(day))
 
 #Heatmap table over Detections
 ggplot(daggHeat, aes(x = Range, y = Scenario)) + 
@@ -196,7 +268,7 @@ ggplot(daggHeat, aes(x = Range, y = Scenario)) +
   theme(legend.title=element_text(face="bold", size=14)) + 
   scale_y_continuous(trans = "reverse")+
   labs(fill="Detections") +
-  facet_grid(cols=vars(FOD))
+  facet_grid(cols=vars(FOD), row=vars(day))
 
 #Heatmap table over Collisions
 ggplot(daggHeat, aes(x = Range, y = Scenario)) + 
@@ -218,10 +290,11 @@ ggplot(daggHeat, aes(x = Range, y = Scenario)) +
   theme(legend.title=element_text(face="bold", size=14)) + 
   scale_y_continuous(trans = "reverse")+
   labs(fill="Collisions") +
-  facet_grid(cols=vars(FOD))
+  facet_grid(cols=vars(FOD), row=vars(day))
 
 #----------------------------   The effect of Training Time
 
+#Make functions
 lower_ci <- function(mean, se, n, conf_level = 0.95){
   lower_ci <- mean - qt(1 - ((1 - conf_level) / 2), n - 1) * se
 }
@@ -229,34 +302,18 @@ upper_ci <- function(mean, se, n, conf_level = 0.95){
   upper_ci <- mean + qt(1 - ((1 - conf_level) / 2), n - 1) * se
 }
 
-
 #Data grouped by Day, FOD and Range
-daggByDFR<-daggByScen %>%group_by(FOD,day,Range)%>%summarize(totalTimeTraining=max(totalTimeTraining),avgTime=mean(Time),ssd = sd(Time),count=n(),se = ssd / sqrt(count))%>%mutate(lower_ci=lower_ci(avgTime,se,count),upper_ci=upper_ci(avgTime,se,count),objectDetected=mean(objectDetected))
+daggByDFR<-daggByScen %>%group_by(FOD,day,Range)%>%summarize(totalTimeTraining=max(totalTimeTraining),avgTime=mean(Time),ssd = sd(Time),count=n(),se = ssd / sqrt(count))%>%mutate(lower_ci=lower_ci(avgTime,se,count),upper_ci=upper_ci(avgTime,se,count))
 
-
-ggplot(daggByScen,aes(x=FOD,y=Time,color=Range))+
-  geom_point(aes(alpha=.1))
-
-ggplot(daggByScen,aes(x=totalTimeTraining,y=Time))+
-  geom_point()
-
-
-ggplot(daggByScen,aes(x=totalTimeTraining,y=Time,color=FOD))+
-  geom_point()+ 
-  geom_smooth(size=0)+ 
-  stat_smooth(aes(color="red"),method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
-  theme_bw()+
-  facet_grid(cols=vars(FOD))
-
-                                                                                         
+#Learning over the three days
 ggplot(daggByDFR,aes(x=totalTimeTraining,y=avgTime,color=factor(Range),shape=factor(Range)))+
   geom_point()+ 
   geom_smooth(size=0)+ 
   geom_errorbar(aes(ymin=lower_ci, ymax=upper_ci))+
   stat_smooth(method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
   theme_bw()+
-  geom_point(aes(y = objectDetected), color = "green")+
-  stat_smooth(aes(y = objectDetected), color="green",method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
+  #geom_point(aes(y = objectDetected), color = "green")+
+  #stat_smooth(aes(y = objectDetected), color="green",method = 'nls', formula = 'y~a*x^b', method.args = list(start= c(a = 1,b=1)),se=FALSE)+
   facet_grid(cols=vars(FOD))
 
 
