97c02a91fb215edde25d19210359675236a4e1cd
Hendrik Knoche <hendrik.knoche@gmail.com>
2020-08-20 15:14:39 +0200

    added material for vibration starts

--- import.R
+++ import.R
@@ -31,7 +31,7 @@ df$objColl<-ifelse(substr(df$Object_collision,1,1)=="B" & df$objBefore=='',1,0)
 
 #count detections
 df$Object_detected<-gsub('null', '', df$Object_detected)
-df$objDetectBefore<-c('',df[1:(nrow(df)-1),]$Object_detected)
+df$objDetectBefore<-c('',df[1:(nrow(df)-1),]$Object_detected) 
 df$objDet<-ifelse(substr(df$Object_detected,1,1)=="B" & df$objDetectBefore=='',1,0)
 
 #Use time stamp to calculate how long a test took
@@ -39,12 +39,25 @@ df$Time_stamp<- as.POSIXct(df$Time_stamp,format="%d/%m/%Y %H:%M:%S")
 df<-df[order(df$Time_stamp),]
 df$timeSinceExpStarted<-time(df$Time_stamp) -min(time(df$Time_stamp))
 
+df$NewTimer<-lag(df$Timer,1)
+df$ScenarioBefore<-lag(df$Scenario,default=99999)
+df$ScenarioStarts<-ifelse(!(df$Scenario==df$ScenarioBefore),1,0)
+df$RunningScenarioCounter <-cumsum(df$ScenarioStarts)
 
 # gsub('_', '-', data1$c)
 # df$te
 # helper$FOD<- as.factor(helper,levels = c("Baseline", "Corridor", "large")
 
 #Make data frame into a .rda file for faster running time
+
+# add consistentTimeline
+
+df$ObjDetID<-cumsum(df$objDet)
+df$RunningTime <- cumsum(df$Time_in_MS*1000)
+
+
+df$ObjDetChangeHlp <- lag(df$Object_detected)
+
 save(df, file='data_all.rda', compress=TRUE)
 
 
